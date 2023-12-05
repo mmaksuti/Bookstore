a164ca8a4b52f7044d70c9bff8694b1813e35c48
NeevinaDervishi <106522748+NevinaDervishi@users.noreply.github.com>
2023-12-05 20:24:02 +0100

    Test AuthorsControllers

---
+++ test/TestLoginController.java
@@ -0,0 +1,9 @@
+package test;
+
+import controllers.LoginController;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestLoginController {
+    private static final String TEST_DATABASE = "testUsersDatabase.dat";
+    private static final String TEST_SESSION = "testLogin.txt";
+}
